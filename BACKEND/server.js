5ba48b7cef4d4b85e4232f23e34ce0848139093b
Rajat Pandey <rajatdevpandey@gmail.com>
2026-03-25 02:01:38 +0530

    feat: add production build scripts and static file serving

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -1,14 +1,16 @@
+import path from "path";
 import express from "express";
 import {app,server} from "./socket/socket.js";
 import dotenv from "dotenv";
 dotenv.config();
+
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
 import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import cookieParser from "cookie-parser";   
 const port= process.env.PORT || 5000;
-
+const __dirname = path.resolve();
 app.use(express.json());
 app.use(cookieParser());
 
@@ -16,6 +18,11 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
 server.listen(port, () => {
     connectToMongoDB();
     console.log(`Server is running on port ${port}`);
